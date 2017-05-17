bcf14790323ae49801ad3cbaf7a53e47e2115636
Robert Wohler Jr <robertwohlerjr@SnowDogMac.local>
2017-05-16 21:28:35 -0500

    Condensed syntax for the http.createServer syntax

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 var http = require("http");
 
-// the onRequest custom function will write out "request received"
-// each time an http request is received.
-function onRequest(request, response){
+// The condensed syntax below represents an
+// anyonymous function (the parameters to the createServer call).
+// This is the equivalent of breaking it out into a separate
+// onRequest style function definition that createServer might execute.
+
+http.createServer(function(request, response){
     console.log("Request received");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
-    response.end(); 
-}
+    response.end();
+}).listen(8888);
 
-// Will write out "server has started" ONLY when the webs server
-// first starts.
-http.createServer(onRequest).listen(8888);
 console.log("Server has started");
